ef2f2b49747e966deb6b4118acd9d6d341f844d9
Tony Kanyamuka <tonykanyamuka@gmail.com>
2025-01-29 21:59:00 +0200

    Navigation :  created navigationBar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,47 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { Tabs } from "expo-router";
+import { FontAwesome5, MaterialIcons } from "@expo/vector-icons";
 
 const TabsLayout = () => {
   return (
-    <View>
-      <Text>TabsLayout</Text>
-    </View>
+    <Tabs
+      screenOptions={{
+        // tabBarShowLabel: false,
+        tabBarActiveBackgroundColor: "#fff",
+        tabBarActiveTintColor: "#a16207",
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="home" size={24} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="discover"
+        options={{
+          title: "Discover",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="explore" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="search" size={24} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 };
 
