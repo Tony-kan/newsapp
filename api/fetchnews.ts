3a3ed30a02cae99f923ea47029ef7f92df8eef3e
Tony Kanyamuka <tonykanyamuka@gmail.com>
2025-01-29 21:10:05 +0200

    Setup : configure tanstack query,created utils functionality, fetchnews functions and endpoints

---
+++ api/fetchnews.ts
@@ -0,0 +1,52 @@
+//👇🏻 base URL
+const apiBaseUrl = "https://newsapi.org/v2";
+
+//👇🏻 breaking news endpoint
+const breakingNewsUrl = `${apiBaseUrl}/top-headlines?country=ng&apiKey=${process
+    .env.EXPO_PUBLIC_NEWS_API_KEY!}`;
+
+//👇🏻 recommended news endpoint
+const recommendedNewsUrl = `${apiBaseUrl}/top-headlines?country=ng&category=business&apiKey=${process
+    .env.EXPO_PUBLIC_NEWS_API_KEY!}`;
+
+//👇🏻 fetch by category  endpoint
+const discoverNewsUrl = (discover: string) =>
+    `${apiBaseUrl}/top-headlines?country=ng&category=${discover}&apiKey=${process
+        .env.EXPO_PUBLIC_NEWS_API_KEY!}`;
+
+//👇🏻 search news endpoint
+const searchNewsUrl = (query: string) =>
+    `${apiBaseUrl}/everything?q=${query}&apiKey=${process.env
+        .EXPO_PUBLIC_NEWS_API_KEY!}`;
+
+//👇🏻 API function call
+const newsApiCall = async (endpoints: string) => {
+    try {
+        const response = await fetch(endpoints);
+        const data = await response.json();
+        return data;
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+//👇🏻 returns breaking news
+export const fetchBreakingNews = async () => {
+    return await newsApiCall(breakingNewsUrl);
+};
+
+//👇🏻 returns recommended news
+export const fetchRecommendedNews = async () => {
+    return await newsApiCall(recommendedNewsUrl);
+};
+
+//👇🏻 returns news based on a category
+export const fetchDiscoverNews = async (discover: string) => {
+    return await newsApiCall(discoverNewsUrl(discover));
+};
+
+//👇🏻 returns search query news
+export const fetchSearchNews = async (query: string) => {
+    const endpoint = searchNewsUrl(query);
+    return await newsApiCall(endpoint);
+};
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,14 @@
 import { Stack } from "expo-router";
-import "./global.css"
+import "./global.css";
+import React from "react";
+import QueryProvider from "@/providers/QueryProvider";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <QueryProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" options={{ title: "Home" }} />
+      </Stack>
+    </QueryProvider>
+  );
 }
